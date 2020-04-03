cb42b170106fa5b35c2ec803476b6b92c78e631e
Concrete18 <michaelericson19@gmail.com>
2020-04-02 22:10:33 -0400

    Fixed missing 0 in Int and decimal list

--- CalculatorScript.py
+++ CalculatorScript.py
@@ -11,7 +11,7 @@ Finished = False
 def PressKey(Key):
     global Equation, TempEq, ShownEQ,  Finished
     print('Pressed '+str(Key))
-    IntsAndDec = [1, 2, 3, 4, 5, 6, 7, 8, 9, '.']
+    IntsAndDec = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.']
     if Key in IntsAndDec:
         TempEq.append(str(Key))
     elif Key == '=':
@@ -31,14 +31,15 @@ def PressKey(Key):
     ShownEQ = Equation + TempEq
     Answer.config(text=''.join(ShownEQ))
     print(ShownEQ)
+    print('')
 
 
 def PressDelete():
     global Equation
     try:
         del Equation[-1]
-        Answer.config(text=''.join(Equation))
-        print('Clear was pressed.')
+        Answer.config(text=''.join(str(Equation)))
+        print('Delete was pressed.')
     except IndexError:
         print('Nothing to Delete')
 
@@ -82,16 +83,18 @@ def PressCalculate():
                 for item in range(3):
                     Equation.pop(OpLo-1)
                 Equation.insert(OpLo-1, OpAnswer)
-            except:
+            except ValueError:
                 print('No ' + Op + ' found.')
-    print(Equation)
-    Answer.config(text=str(CalcAnswer))
+    Answer.config(text='= ' + str(Equation[0]))
     Finished = True
     print(CalcAnswer)
 
 
 # Defaults for Interface
 Background = 'White'
+Button1bg = 'White'
+Button2bg = 'light sky blue'
+Button3bg = 'gray80'
 BoldBaseFont = "Arial Bold"
 BaseFont = "Arial"
 FontColor = "Black"
@@ -113,58 +116,58 @@ AnswerFrame.grid(columnspan=4, row=0)
 Answer = Label(AnswerFrame, text='0', anchor="w", width=14, font=(BoldBaseFont, 20), fg='white', bg='gray12')
 Answer.grid(columnspan=4, row=0)
 
-OneKey = Button(text='1', command=partial(PressKey, int(1)), font=BaseFont, width=ButtonWidth, height=ButtonHeight)
+OneKey = Button(text='1', command=partial(PressKey, 1), bg=Button1bg, font=BaseFont, width=ButtonWidth, height=ButtonHeight)
 OneKey.grid(column=0, row=4, padx=ButtonPaddingX, pady=ButtonPaddingY)
 
-TwoKey = Button(text='2', command=partial(PressKey, 2), font=BaseFont, width=ButtonWidth, height=ButtonHeight)
+TwoKey = Button(text='2', command=partial(PressKey, 2), bg=Button1bg, font=BaseFont, width=ButtonWidth, height=ButtonHeight)
 TwoKey.grid(column=1, row=4, padx=ButtonPaddingX, pady=ButtonPaddingY)
 
-ThreeKey = Button(text='3', command=partial(PressKey, 3), font=BaseFont, width=ButtonWidth, height=ButtonHeight)
+ThreeKey = Button(text='3', command=partial(PressKey, 3), bg=Button1bg, font=BaseFont, width=ButtonWidth, height=ButtonHeight)
 ThreeKey.grid(column=2, row=4, padx=ButtonPaddingX, pady=ButtonPaddingY)
 
-FourKey = Button(text='4', command=partial(PressKey, 4), font=BaseFont, width=ButtonWidth, height=ButtonHeight)
+FourKey = Button(text='4', command=partial(PressKey, 4), bg=Button1bg, font=BaseFont, width=ButtonWidth, height=ButtonHeight)
 FourKey.grid(column=0, row=3, padx=ButtonPaddingX, pady=ButtonPaddingY)
 
-FiveKey = Button(text='5', command=partial(PressKey, 5), font=BaseFont, width=ButtonWidth, height=ButtonHeight)
+FiveKey = Button(text='5', command=partial(PressKey, 5), bg=Button1bg, font=BaseFont, width=ButtonWidth, height=ButtonHeight)
 FiveKey.grid(column=1, row=3, padx=ButtonPaddingX, pady=ButtonPaddingY)
 
-SixKey = Button(text='6', command=partial(PressKey, 6), font=BaseFont, width=ButtonWidth, height=ButtonHeight)
+SixKey = Button(text='6', command=partial(PressKey, 6), bg=Button1bg, font=BaseFont, width=ButtonWidth, height=ButtonHeight)
 SixKey.grid(column=2, row=3, padx=ButtonPaddingX, pady=ButtonPaddingY)
 
-SevenKey = Button(text='7', command=partial(PressKey, 7), font=BaseFont, width=ButtonWidth, height=ButtonHeight)
+SevenKey = Button(text='7', command=partial(PressKey, 7), bg=Button1bg, font=BaseFont, width=ButtonWidth, height=ButtonHeight)
 SevenKey.grid(column=0, row=2, padx=ButtonPaddingX, pady=ButtonPaddingY)
 
-EightKey = Button(text='8', command=partial(PressKey, 8), font=BaseFont, width=ButtonWidth, height=ButtonHeight)
+EightKey = Button(text='8', command=partial(PressKey, 8), bg=Button1bg, font=BaseFont, width=ButtonWidth, height=ButtonHeight)
 EightKey.grid(column=1, row=2, padx=ButtonPaddingX, pady=ButtonPaddingY)
 
-NineKey = Button(text='9', command=partial(PressKey, 9), font=BaseFont, width=ButtonWidth, height=ButtonHeight)
+NineKey = Button(text='9', command=partial(PressKey, 9), bg=Button1bg, font=BaseFont, width=ButtonWidth, height=ButtonHeight)
 NineKey.grid(column=2, row=2, padx=ButtonPaddingX, pady=ButtonPaddingY)
 
-ZeroKey = Button(text='0', command=partial(PressKey, 0), font=BaseFont, width=ButtonWidth, height=ButtonHeight)
+ZeroKey = Button(text='0', command=partial(PressKey, 0), bg=Button1bg, font=BaseFont, width=ButtonWidth, height=ButtonHeight)
 ZeroKey.grid(column=1, row=5, padx=ButtonPaddingX, pady=ButtonPaddingY)
 
-PeriodKey = Button(text='.', command=partial(PressKey, '.'), font=BaseFont, width=ButtonWidth, height=ButtonHeight)
+PeriodKey = Button(text='.', command=partial(PressKey, '.'), bg=Button1bg, font=BaseFont, width=ButtonWidth, height=ButtonHeight)
 PeriodKey.grid(column=2, row=5, padx=ButtonPaddingX, pady=ButtonPaddingY)
 
-PlusKey = Button(text='+', command=partial(PressKey, '+'), font=BaseFont, width=ButtonWidth, height=ButtonHeight)
+PlusKey = Button(text='+', command=partial(PressKey, '+'), bg=Button2bg, font=BaseFont, width=ButtonWidth, height=ButtonHeight)
 PlusKey.grid(column=3, row=4, padx=ButtonPaddingX, pady=ButtonPaddingY)
 
-MinusKey = Button(text='-', command=partial(PressKey, '-'), font=BaseFont, width=ButtonWidth, height=ButtonHeight)
+MinusKey = Button(text='-', command=partial(PressKey, '-'), bg=Button2bg, font=BaseFont, width=ButtonWidth, height=ButtonHeight)
 MinusKey.grid(column=3, row=3, padx=ButtonPaddingX, pady=ButtonPaddingY)
 
-DivideKey = Button(text='/', command=partial(PressKey, '/'), font=BaseFont, width=ButtonWidth, height=ButtonHeight)
+DivideKey = Button(text='/', command=partial(PressKey, '/'), bg=Button2bg, font=BaseFont, width=ButtonWidth, height=ButtonHeight)
 DivideKey.grid(column=3, row=1, padx=ButtonPaddingX, pady=ButtonPaddingY)
 
-MultiplyKey = Button(text='X', command=partial(PressKey, '*'), font=BaseFont, width=ButtonWidth, height=ButtonHeight)
+MultiplyKey = Button(text='X', command=partial(PressKey, '*'), bg=Button2bg, font=BaseFont, width=ButtonWidth, height=ButtonHeight)
 MultiplyKey.grid(column=3, row=2, padx=ButtonPaddingX, pady=ButtonPaddingY)
 
-ExpoKey = Button(text='^', command=partial(PressKey, '**'), font=BaseFont, width=ButtonWidth, height=ButtonHeight)
+ExpoKey = Button(text='^', command=partial(PressKey, '**'), bg=Button2bg,  font=BaseFont, width=ButtonWidth, height=ButtonHeight)
 ExpoKey.grid(column=2, row=1, padx=ButtonPaddingX, pady=ButtonPaddingY)
 
-DeleteKey = Button(text='Delete', command=PressDelete, font=BaseFont, width=ButtonWidth, height=ButtonHeight)
+DeleteKey = Button(text='Delete', command=PressDelete, bg=Button3bg, font=BaseFont, width=ButtonWidth, height=ButtonHeight)
 DeleteKey.grid(column=1, row=1, padx=ButtonPaddingX, pady=ButtonPaddingY)
 
-ClearKey = Button(text='Clear', command=PressClear, font=BaseFont, width=ButtonWidth, height=ButtonHeight)
+ClearKey = Button(text='Clear', command=PressClear, bg=Button3bg, font=BaseFont, width=ButtonWidth, height=ButtonHeight)
 ClearKey.grid(column=0, row=1, padx=ButtonPaddingX, pady=ButtonPaddingY)
 
 CalculateKey = Button(text='=', command=partial(PressKey, '='), font=BaseFont, width=ButtonWidth, height=ButtonHeight)
